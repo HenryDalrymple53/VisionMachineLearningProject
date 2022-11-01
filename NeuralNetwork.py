806cf47a282a244e571a905fa1d35cf24a84d1e0
HenryDalrymple53 <henrydalrymple53@gmail.com>
2022-11-01 14:58:26 -0700

    Update
    Update for better flexibility in hidden layer depth and number of hidden layers, along with optional leaky relu implementation

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,96 +1,202 @@
-#Framework for a single hidden layer NN, with variable neurons in hidden layer. 
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 import numpy as np
-import cv2
-from scipy.special import expit
-from keras.datasets import mnist
-
-
+import pandas as pd
+import random
+#Potentially use leaky relu for weights and bias, and use sigmoid for output normalization.
+#Avoiding vanishing gradient while keeping outputs scaled between -1 and 1
 class NeuralNetwork:
-    inputWeights = []
-    outputWeights = []
-    cellInfo = []
+    #Initializing variables
+    weights = []
+    bias = []
+    answers = []
     input = []
+    hlVals = []
+    hlSig = []
     output = []
-    (train_X, train_y), (test_X, test_y) = mnist.load_data()
-    def __init__(self, outputLength,layerLength):
-        print(f"Test: {self.train_X[0]}")
-        # ** Initialize bias here as well
-        output = self.output
-        cellInfo = self.cellInfo
-        outputWeights = self.outputWeights
-        inputWeights = self.inputWeights
-        input = self.input
-        for o in range(outputLength):
-            output.append(0)
-        for cell in range(layerLength):
-            
-            # Cell info: [0] = value, [1] = bias
-            cellInfo.append([0]*2)
-            inputWeights.append([0]*len(input))
-            for iw in range(len(input)):
-                
-                inputWeights[cell][iw] = (np.random.rand()*2)-1
-        
-        for cell in range(len(output)):
-            outputWeights.append([0]*len(cellInfo))
-            for ow in range(len(cellInfo)):
-                
-                outputWeights[cell][ow] = (np.random.rand()*2)-1
-        
-        self.inputWeights = inputWeights
-        self.outputWeights = outputWeights
-        self.cellInfo = cellInfo
-        self.input = input
-        self.output = output
-        
-        
-    def feedforward(self):
-        cellInfo = self.cellInfo
-        output = self.output
-        outputWeights = self.outputWeights
-        inputWeights = self.inputWeights
-        input = self.input
-        ind = 0
-        for cell in range(len(cellInfo)):
-            # Cell info: [0] = value, [1] = bias
-            sum = 0
-            counter = 0
-            for iw in inputWeights[cell]:
-                sum += iw*input[counter]
-                counter+=1
-            cellInfo[cell][0] = 1/(1+np.exp(-sum+cellInfo[cell][1]))
-            
-        for c in range(len(output)):
-            # Cell info: [0] = value, [1] = bias
-            sum = 0
-            counter = 0
-            
-            for ow in outputWeights[c]: 
-                sum += ow*cellInfo[counter][0]
-                counter+=1
-            output[c] = (1/(1+np.exp(-sum)))
-        largest = 0
-        ind = 0
-        for x in output:
-            if x>output[largest]:
-                largest = ind
-            ind+=1
-        return largest
+    DCoW = []
+    DCoB = []
+    sumCW = []
+    sumCB = []
+    only123 = []
+    options = []
     
+    correct = 0
+    #Used for measuring accuracy
+    correctList = [0,0,0,0,0,0,0,0,0,0]
+    incorrectList = [0,0,0,0,0,0,0,0,0,0]
+    #Reading in Data and shuffling it, no partition between test and train sets, as train isn't perfectly
+    #replicable for what I am working with
+    data = pd.read_csv("C:\\PythonProjects\\DataFiles\\train.csv")
+    data = np.array(data[0:50000])
+    random.shuffle(data)
+    i = 0
+    #Returning answers for the associated array inputs
+    for x in data:
+        answers.append(x[0])
+        if(x[0]==1 or x[0]==2 or x[0]==3 or x[0]==4):
+            only123.append(i)
+        i+=1
+    
+    outputLength = 10
+    hiddenLayerLength = 0
+    #Initialize weights and bias for dimensionality given
+    def __init__(self, layerLengths):
+        prev = 784
+        
+        for i in range(0,len(layerLengths)):
+            self.weights.append((np.random.normal(size=(layerLengths[i], prev)) * np.sqrt(2/layerLengths[i])))
+            prev = layerLengths[i]
+            self.bias.append((np.random.normal(size=(prev, 1)) * np.sqrt(2/prev)))
+        self.weights.append((np.random.normal(size=(self.outputLength, prev)) * np.sqrt(2/self.outputLength)))
+        self.bias.append(np.random.normal(size=(self.outputLength, 1)) * np.sqrt(2/self.outputLength))
+    
+    def sigmoid(self, x): #Sigmoid squishes any number given to it
+        sig = 1 / (1 + np.exp(-x))
+        return sig
+    def sigdir(self,x): #Sigmoid der
+        return self.sigmoid(x) * (1- self.sigmoid(x))
+
+    def leaky_Relu(self,x): #Unused leaky_relu
+        count = 0
+        for val in x:
+             x[count] = val*0.01 if val < 0 else val
+             count+=1
+        return x
+
+    def  leaky_Relu_Derivative(self,x): # Unused leaky_relu derv
+        count = 0
+        for val in x:
+            x[count] = 0.01 if val < 0 else 1
+            count+=1
+        return x
+    def forwardProp(self):# Run through the application, starting at the input set, using matrix multiplication
+
+        #Start at input, multiply through weights, end at output. Similar structure to top
+        self.hlVals.append(self.weights[0].dot(self.input) + self.bias[0])
+        self.hlSig.append(self.sigmoid(self.hlVals[0]))
+        for i in range(1,len(self.weights)):
+            self.hlVals.append(self.weights[i].dot(self.hlSig[i-1]) + self.bias[i])
+            self.hlSig.append(self.sigmoid(self.hlVals[i]))
+            
+
+    def backProp(self, ind):#Backpropigate through the values, finding gradient descent curves for each
+                            #weight and bias
+        #Cost of the function
+        prev = 2*(self.hlSig[len(self.hlSig)-1]-self.responseCleaner(self.answers[ind]))
+        for i in range(len(self.hlSig)-1,0,-1):
+            
+            DAoZn = self.sigdir(prev)   
+            DZoWn = np.reshape(self.hlSig[i-1],(1,len(self.hlSig[i-1])))
+            DZnA1 = np.reshape(self.weights[i],(len(self.hlSig[i-1]),len(self.hlSig[i])))
+            dot = DAoZn*prev
+            self.DCoW.append(dot*DZoWn)
+            self.DCoB.append(dot)
+            prev = DZnA1.dot(dot)
+        
+        DAoZn = self.sigdir(prev)   #5,1
+        DZoWn = np.reshape(self.input,(1,len(self.input))) #1,784
+        DZnA1 = np.reshape(self.weights[0],(len(self.input),len(self.hlSig[0]))) #784,5
+        self.DCoW.append((DAoZn*prev)*DZoWn)
+        
+        self.DCoB.append(DAoZn*prev)
+        
+            
+    def responseCleaner(self, ans): #Finds the most confident answer given, and creates a binary array 
+                                    #With 1 at greatest value
+        
+        retArr = []
+        for x in range(0,10):
+            if(x==ans):
+                retArr.append(1)
+            else :
+                retArr.append(0)
+        guess = np.argmax(self.hlSig[len(self.hlSig)-1])
+        
+        if(guess==ans):
+            self.correctList[ans]+=1
+            self.correct +=1
+        else :
+            self.incorrectList[ans]+=1
+        return np.reshape(retArr,(len(retArr),1))
+
+    def update(self, alpha): #This updates the values after the length of the batch.
+        x = len(self.weights)-1
+        for i in range(0,len(self.weights)):
+            
+            self.weights[i] -= np.multiply(self.sumCW[x],alpha)
+            x-=1
+        y = len(self.weights)-1
+        for i in range(0,len(self.weights)):
+            
+            self.bias[i] -= np.multiply(self.sumCB[y],alpha)
+            y-=1
+        self.sumCB.clear()
+        self.sumCW.clear()
+        
+
+    def train(self,iterations, alpha, epoch, batch): #Putting it all together
+        data = []
+       
+        accuracy = []
+        for x in range(0,epoch):
+            print(f"Start Val: {self.only123[0:5]}")
+            print(self.weights[len(self.weights)-1])
+            for i in range(0,iterations):
+                
+                self.trainInput(i)
+                self.forwardProp()
+                
+                self.backProp(i)
+                #The batch collects the weights over a certain # of iterations, then applying them at the end
+                #of the batch.
+                if(i%batch==0):
+                    self.sumCW = self.DCoW #Zeroes out the changes
+                    self.sumCB = self.DCoB 
+                elif(i%batch==batch-1):
+                    self.update(alpha) #Updates changes
+                else:
+                    self.summation(alpha) #Adds changes
+                #At i%10==9,  
+            
+                self.clear()
+                if(i%1000==0):
+                    
+                    accuracy.append(self.correct/1000)
+                    print(self.correct/1000)
+                    self.correct = 0
+            print(f"Next Epoch ----------------------")
+            print(self.weights[len(self.weights)-1])
+        print(f"Correct Guesses: {self.correctList}")
+        print(f"Incorrect Guesses: {self.incorrectList}")
+        
     def updateInput(self, input):
         self.input = input
-    def inputChecker(self):
-        input = self.input
-        zeroCount = 0
-        oneCount = 1
-        for x in input:
+    def clear(self):
+        self.hlVals.clear()
+        self.hlSig.clear()
+        self.DCoW.clear()
+        self.DCoB.clear()
+    def summation(self, alpha): #Is called throughout the length of the batch to add all of the weights and biases
+        
+        for i in range(0,len(self.weights)):
             
-            if(x == 0):
-                zeroCount += 1
+            self.sumCW[i] += np.multiply(self.DCoW[i],alpha)
+            
+        
+        for i in range(0,len(self.weights)):
+            
+            self.sumCB[i] += np.multiply(self.DCoB[i],alpha)
+            
+    def trainInput(self, ind): # Changes values of Mnist from 0-255 to 0-1
+        
+        self.input =  np.reshape(self.data[ind][1:785],(784,1))
+        for i in range(0,len(self.input)):
+            val = self.input[i]
+            if(val>=230):
+                self.input[i]=1
             else :
-                oneCount += 1
-        print(f"zeroCount: {zeroCount}")
-        print(F"oneCount: {oneCount}")
-    
+                self.input[i]=0
+    def guessNum(self, input): #Meant to be used with camera information
+        self.forwardProp()
+        guess = np.argmax(self.hlSig[len(self.hlSig)-1])
+        print(guess)
--- opencvTest.py
+++ opencvTest.py
@@ -1,48 +1,54 @@
-from bz2 import compress
 import numpy as np
 import cv2
-import multiprocessing
 import sys
-from scipy.special import expit
 
-sys.path.append("C:\PythonProjects")
-from NeuralNetwork import *
-
+#Initializing variables
+sys.path.append("C/PythonProjects")
+from betterNeuralNetwork import *
 capture=cv2.VideoCapture(0)
-cascade_classifier = cv2.CascadeClassifier("C:\\Users\\henry\\AppData\\Local\\Programs\\Python\\Python310\\Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_alt.xml")
 r,f = capture.read()
-#height, width = f.shape[:2]
 height = 480
 width = 640
-hiddenLayerLength = 4
 compression_size = 28
-nn = NeuralNetwork(10,4)
 
+data = pd.read_csv("C:\\PythonProjects\\DataFiles\\train.csv")
+data = np.array(data)
+#nn.returnWeights()hi
 (centerX, centerY) = (width // 2, height // 2)
-
 print('Image height: ', height)
 print('Image width: ', width)
 print('Center location: ', (centerY, centerX))
 gray = cv2.cvtColor(f, cv2.COLOR_RGB2GRAY)
-gVal = gray[centerX,centerY]
+gVal = gray[centerX,centerY] # Gets the Color at the middle of the screen, helps with calibrating in different lighting
 print(gVal)
+#Initializing NeuralNetwork
+nn = NeuralNetwork([20,20])
+#Training fnn
+nn.train(40000,.25,1,10)
 
-def translate(thresh):
+
+def translate(thresh): # Translates pixels from 0 or 255 to 0 or 1
     pixels = []
     for y in thresh:
         for x in y:
-            pixels.append(x)
+            if(x==255):
+                pixels.append(0)
+            else:
+                pixels.append(1)
             
     return pixels
 
 while True:
     
-    ret,frame=capture.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-    ret, thresh = cv2.threshold(gray,int(gVal-10),int(gVal+10),cv2.THRESH_BINARY)
-    gaussian = cv2.GaussianBlur(thresh, (5, 5), 10)
-    
+    ret,frame=capture.read()#Gets camera video
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)#Gray scales video
+    ret, thresh = cv2.threshold(gray,int(gVal-10),int(gVal+10),cv2.THRESH_BINARY) #Thresholds based off previous value given
+    gaussian = cv2.GaussianBlur(thresh, (5, 5), 10)# Blurs the image, removing some small artefacts
+                                                   # That may disrupt imaging
+    #CV2 function find contours finds the edges of objects that it detects. in this case,
+    #it is the blurred gray scale image
     contours,hierarchy =  cv2.findContours(gaussian,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
     
     largest = 0
     ind = 0
@@ -51,19 +57,21 @@ while True:
 
     if(len(contours) > 0):
         
-        for x in contours:
+        for x in contours: #For all of the contours, find the largest and get it's index
             
             if(cv2.contourArea(contours[ind]) > cv2.contourArea(contours[largest])):
                 largest = ind
             ind+=1
             
-        
+        #Draws the largest contour
         cv2.drawContours(frame, contours[largest], -1, (255,0,0),5)
         left = contours[largest][0][0][0]
         right = contours[largest][0][0][0]
         top = contours[largest][0][0][1]
         bot = contours[largest][0][0][1]
-        for x in contours[largest]:
+        # For all the points for the largest contour, find the outermost bounds that the contour goes
+        for x in contours[largest]: 
+            
             
             xPos = x[0][0]
             yPos = x[0][1]
@@ -75,31 +83,20 @@ while True:
                 top = yPos
             if(yPos < bot):
                 bot = yPos
+        
         cv2.rectangle(frame,(left,bot),(right,top),(0,255,0),3)
-        frame = frame[bot:top,left:right]             #           bot    top   left  right
-        threshRez = thresh[bot:top,left:right]
-        dim = (compression_size,compression_size)
-        frame = cv2.resize(frame,dim)
-        threshRez = cv2.resize(threshRez,dim)
-        pixels = translate(threshRez)
-        nn.updateInput(pixels)
-        #print(nn.feedforward())
         
-        #q1 = multiprocessing.Process(target = translate, args = [(top+bot)/2,top,(right+left)/2,right,thresh]) #Top Right
-        #q1.start()
-        #q2 = multiprocessing.Process(target = translate, args = [(top+bot)/2,top,left,(right+left)/2]) #Top Left
-       # q2.start()
-       # q3 = multiprocessing.Process(target = translate, args = [bot,(top+bot)/2,left,(right+left)/2]) #Bot Left
-       # q3.start()
-       # q4 = multiprocessing.Process(target = translate, args = [bot,(top+bot)/2,(right+left)/2,right]) #Bot Right
-
-       # q4.start()
-       # q1.join()
-       # q2.join()
-       # q3.join()
-       # q4.join()
+        frame = frame[bot:top,left:right]#Resizes a frame to be the dimensions of the tracked object    
+        threshRez = thresh[bot:top,left:right]#Binary image scaled to these dimensions
+        dim = (compression_size,compression_size)# Dimensions of the wanted compression, for MNIST 28x28
+        frame = cv2.resize(frame,dim)# Resizes frame to the dimensions
+        threshRez = cv2.resize(threshRez,dim)#Resizes the thresholded image
+        pixels = translate(threshRez)#Highlights the negative space in the image, in this case hopefully the number
+        nn.updateInput(np.reshape(pixels,(len(pixels),1))) #Reshape and update input for NeuralNetwork
+        
+         
         cv2.imshow('Color',threshRez)
-        
+    #Exits camera when q is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     
@@ -108,9 +105,5 @@ while True:
    
 capture.release()
 cv2.destroyAllWindows()
-
-#faces = cascade_classifier.detectMultiScale(image_grey, minSize=(30, 30))
- #for (x, y, w, h) in faces:q
-    #    cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),5)
 
 
