278b0e22bc8da7d646d52930d32f0e671ae0b6f7
HenryDalrymple53 <henrydalrymple53@gmail.com>
2023-03-03 09:17:45 -0800

    Update PytorchNN.py

--- PytorchNN.py
+++ PytorchNN.py
@@ -22,69 +22,69 @@ class NeuralNet(nn.Module):
         return out
 
 
-
-data = pd.read_csv("C:\\PythonProjects\\DataFiles\\train.csv")
-camera_in = []
-input_size = 784 # 28x28
-hidden_size = 500
-num_classes = 10
-num_epochs = 2
-batch_size = 100
-learning_rate = 0.001 
-
-
-train_dataset = torchvision.datasets.MNIST(root='./data', 
-                                            train=True, 
-                                       transform=transforms.ToTensor(),  
-                                           download=True)
-test_dataset = torchvision.datasets.MNIST(root='./data', 
-                                           train=False, 
-                                           transform=transforms.ToTensor())
-
-
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
-                                            batch_size=batch_size, 
-                                            shuffle=True)
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
-                                           batch_size=batch_size, 
-                                           shuffle=False)
-
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-model = NeuralNet(input_size, hidden_size, num_classes).to(device)
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
-n_total_steps = len(train_loader)
-for epoch in range(num_epochs):
-    for i, (images, labels) in enumerate(train_loader):  
-        # origin shape: [100, 1, 28, 28]
-        # resized: [100, 784]
-        images = images.reshape(-1, 28*28).to(device)
-        labels = labels.to(device)
-        # Forward pass
-        outputs = model(images)
-        loss = criterion(outputs, labels)
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        if (i+1) % 100 == 0:
-            print (f'Epoch [{epoch+1}/{num_epochs}], Step[{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
-with torch.no_grad():
-    n_correct = 0
-    n_samples = 0
-    for images, labels in test_loader:
-        images = images.reshape(-1, 28*28).to(device)
-        labels = labels.to(device)
-        outputs = model(images)
-        # max returns (value ,index)
-        _, predicted = torch.max(outputs.data, 1)
-        n_samples += labels.size(0)
-        n_correct += (predicted == labels).sum().item()
-    acc = 100.0 * n_correct / n_samples
-    print(f'Accuracy of the network on the 10000 test images: {acc} %')
-
-def updateInput(input):
-    camera_in = input
+class PyTorchNN():
+    data = pd.read_csv("C:\\PythonProjects\\DataFiles\\train.csv")
+    camera_in = []
+    input_size = 784 # 28x28
+    hidden_size = 500
+    num_classes = 10
+    num_epochs = 2
+    batch_size = 100
+    learning_rate = 0.001 
+    
+    
+    train_dataset = torchvision.datasets.MNIST(root='./data', 
+                                                train=True, 
+                                           transform=transforms.ToTensor(),  
+                                               download=True)
+    test_dataset = torchvision.datasets.MNIST(root='./data', 
+                                               train=False, 
+                                               transform=transforms.ToTensor())
+    
+    
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
+                                                batch_size=batch_size, 
+                                                shuffle=True)
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
+                                               batch_size=batch_size, 
+                                               shuffle=False)
+    
+    
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
+    model = NeuralNet(input_size, hidden_size, num_classes).to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    
+    n_total_steps = len(train_loader)
+    for epoch in range(num_epochs):
+        for i, (images, labels) in enumerate(train_loader):  
+            # origin shape: [100, 1, 28, 28]
+            # resized: [100, 784]
+            images = images.reshape(-1, 28*28).to(device)
+            labels = labels.to(device)
+            # Forward pass
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            if (i+1) % 100 == 0:
+                print (f'Epoch [{epoch+1}/{num_epochs}], Step[{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
+    with torch.no_grad():
+        n_correct = 0
+        n_samples = 0
+        for images, labels in test_loader:
+            images = images.reshape(-1, 28*28).to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            # max returns (value ,index)
+            _, predicted = torch.max(outputs.data, 1)
+            n_samples += labels.size(0)
+            n_correct += (predicted == labels).sum().item()
+        acc = 100.0 * n_correct / n_samples
+        print(f'Accuracy of the network on the 10000 test images: {acc} %')
+    
+    def updateInput(input):
+        camera_in = input
